5660ba3b91114bfea8f32c41ffe94531df5411ca
Emmanuel UBEDA <ubeda.civico.manuel@gmail.com>
2025-05-17 16:50:54 +0200

    my_forth_commit

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -3,10 +3,11 @@ select
     order_id as order_id_x,
     user_name as user_id_x,
     order_status_x,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at_x,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_x,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_x,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_x,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
+    DATETIME(order_date, "Europe/Paris") AS order_created_at_y,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_y,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_y
+>>>>>>> 106f1d4 (my_forth_commit)
 
 from {{ source('sales_database', 'order') }}
