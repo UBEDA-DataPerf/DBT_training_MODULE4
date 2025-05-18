9c37bc06dc60c0a9df24946ec390a6180ff708bb
EMMANUEL UBEDA <ubeda.civico.manuel@gmail.com>
2025-05-18 16:21:10 +0000

    modification models/staging/sales_database/stg_sales_database__order.sql

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -8,6 +8,6 @@ select
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
     DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
     DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_y
->>>>>>> 106f1d4 (my_forth_commit)
+
 
 from {{ source('sales_database', 'order') }}
